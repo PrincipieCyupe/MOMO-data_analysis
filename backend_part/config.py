c339dbbbfa025cf66a4ffa93579748e93fbcc77d
PrincipieCyupe <p.cyubahiro@alustudent.com>
2025-06-13 15:47:05 -0700

    Front-end and Back-end for MoMo Data Analysis

---
+++ backend_part/config.py
@@ -0,0 +1,7 @@
+# database credentials to be used in app.py while fetching the data into it
+db_config = {
+    "host" : "localhost",
+    "user" : "PrincipieSQL",
+    "password" : "0798200584",
+    "database" : "test"
+}
